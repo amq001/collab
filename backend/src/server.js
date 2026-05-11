f3c934a5dda565c7766943d5f35f795a458c588f
Aqib Mehtab <aqibmehtab001@gmail.com>
2026-05-11 23:49:00 +0500

    Zustand auth setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
+import cors from 'cors';
 
 // Initialize Express app
 const app = express();
@@ -13,6 +14,10 @@ const PORT = ENV.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true
+}));
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,12 +1,37 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-    authUser: {name: "John Doe", _id: "123", age: 30},
-    isLoggedIn: false,
-    isLoading: false,
+    authUser: null,
+    isCheckingAuth: true,
+    isSigningUp: true,
+    // isLoading: false,
 
-    login: () => {
-        console.log("We just logged in")
-        set({isLoggedIn: true, loading: true})
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get('/auth/check')
+            set({authUser: res.data})
+        } catch (error) {
+            console.log("Error in authCheck: ", error)
+            set({authUser: null})
+        } finally {
+            set({isCheckingAuth: false})
+        }
+    },
+
+    signUp: async () => {
+        set({isSigningUp: true})
+        try {
+            const res = await axiosInstance.post('/auth/signup',data)
+            set({authUser: res.data})
+
+            toast.success("Signup successful!")
+        } catch (error) {
+            console.log("Error in authCheck: ", error)
+            set({authUser: null})
+        } finally {
+            set({isSigningUp: false})
+        }
     }
 }))
